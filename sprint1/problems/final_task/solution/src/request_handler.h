0547a023f3eada0cb24c4b66de5f41ff6c36ec9f
Daniil Shvalov <shvalovich@gmail.com>
2022-12-15 13:30:51 +0300

    refactor: redesign request_handler

--- sprint1/problems/final_task/solution/src/request_handler.h
+++ sprint1/problems/final_task/solution/src/request_handler.h
@@ -30,82 +30,86 @@ class RequestHandler {
 
     template <typename Body, typename Allocator, typename Send>
     void operator()(HttpRequest<Body, Allocator>&& req, Send&& send) {
-        std::string_view target = req.target();
-        std::string_view maps_api_uri = "/api/v1/maps";
+        const auto make_json_response = [&](http::status status,
+                                            std::string_view body) {
+            HttpResponse<Body, Allocator> response(status, req.version());
 
-        if (target.starts_with(maps_api_uri)) {
-            if (target == maps_api_uri) {
-                HandleMapsListRequest(std::move(req), std::move(send));
-            } else {
-                HandleMapRequest(std::move(req), std::move(send));
+            response.set(http::field::content_type,
+                         ContentType::APPLICATION_JSON);
+            response.body() = body;
+            response.content_length(body.size());
+            response.keep_alive(req.keep_alive());
+
+            return response;
+        };
+
+        const auto handle_bad_request = [&]() {
+            return make_json_response(http::status::bad_request,
+                                      json::serialize(json::value{
+                                          {"code", "badRequest"},
+                                          {"message", "Bad request"},
+                                      }));
+        };
+
+        const auto get_map_info = [&](std::string map_id) {
+            using namespace std::literals;
+
+            const model::Map* map =
+                game_.FindMap(model::Map::Id(std::move(map_id)));
+
+            if (!map) {
+                return make_json_response(http::status::not_found,
+                                          json::serialize(json::value{
+                                              {"code", "mapNotFound"},
+                                              {"message", "Map not found"},
+                                          }));
             }
-        } else if (target.starts_with("/api/")) {
-            HandleUnknownRequest(std::move(req), std::move(send));
+
+            return make_json_response(
+                http::status::ok,
+                json::serialize(json_serializer::SerializeMapInfo(*map)));
+        };
+
+        const auto get_maps_list = [&]() {
+            return make_json_response(
+                http::status::ok,
+                json::serialize(
+                    json_serializer::SerializeMapsList(game_.GetMaps())));
+        };
+
+        std::string_view target = req.target();
+        const std::string_view api_uri = "/api";
+        std::string_view maps_uri = "/v1/maps";
+
+        if (req.method() != http::verb::get) {
+            return send(handle_bad_request());
         }
+
+        if (!target.starts_with(api_uri)) {
+            return send(handle_bad_request());
+        }
+
+        target.remove_prefix(api_uri.size());
+
+        if (!target.starts_with(maps_uri)) {
+            return send(handle_bad_request());
+        }
+
+        target.remove_prefix(maps_uri.size());
+
+        if (target.empty() || target == "/") {
+            return send(get_maps_list());
+        }
+
+        if (target.front() != '/') {
+            return send(handle_bad_request());
+        }
+
+        return send(get_map_info(
+            std::string{target.substr(1, target.find_last_not_of('/'))}));
     }
 
    private:
-    template <typename Body, typename Allocator>
-    static HttpResponse<Body, Allocator> MakeJsonResponse(http::status status,
-                                                          unsigned version,
-                                                          std::string_view body,
-                                                          bool keep_alive) {
-        HttpResponse<Body, Allocator> response(status, version);
-
-        response.set(http::field::content_type, ContentType::APPLICATION_JSON);
-        response.body() = body;
-        response.content_length(body.size());
-        response.keep_alive(keep_alive);
-
-        return response;
-    }
-
-    template <typename Body, typename Allocator, typename Send>
-    void HandleMapsListRequest(HttpRequest<Body, Allocator>&& req,
-                               Send&& send) {
-        std::string body = json::serialize(
-            json_serializer::SerializeMapsList(game_.GetMaps()));
-
-        send(MakeJsonResponse<Body, Allocator>(http::status::ok, req.version(),
-                                               body, req.keep_alive()));
-    }
-
-    template <typename Body, typename Allocator, typename Send>
-    void HandleMapRequest(HttpRequest<Body, Allocator>&& req, Send&& send) {
-        using namespace std::literals;
-
-        std::string map_id{req.target().substr("/api/v1/maps/"sv.size())};
-        const model::Map* map = game_.FindMap(model::Map::Id(map_id));
-
-        if (!map) {
-            std::string body = json::serialize(json::value{
-                {"code", "mapNotFound"},
-                {"message", "Map not found"},
-            });
-
-            send(MakeJsonResponse<Body, Allocator>(http::status::not_found,
-                                                   req.version(), body,
-                                                   req.keep_alive()));
-        } else {
-            std::string body =
-                json::serialize(json_serializer::SerializeMapInfo(*map));
-
-            send(MakeJsonResponse<Body, Allocator>(
-                http::status::ok, req.version(), body, req.keep_alive()));
-        }
-    }
-
-    template <typename Body, typename Allocator, typename Send>
-    void HandleUnknownRequest(HttpRequest<Body, Allocator>&& req, Send&& send) {
-        std::string body = json::serialize(json::value{
-            {"code", "badRequest"},
-            {"message", "Bad request"},
-        });
-
-        send(MakeJsonResponse<Body, Allocator>(
-            http::status::bad_request, req.version(), body, req.keep_alive()));
-    }
-
     model::Game& game_;
 };
 
