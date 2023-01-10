6e4bbc6656b53a4882f1bd0133e5eafc2abc99ee
Daniil Shvalov <shvalovich@gmail.com>
2023-01-10 18:52:16 +0300

    feat: add move players solution

---
+++ sprint2/problems/move_players/solution/src/model/game.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "model/map.h"
+#include "utils/tagged.h"
+
+#include <vector>
+
+namespace model {
+
+class Game {
+  public:
+    using Maps = std::vector<Map>;
+
+    void AddMap(Map map);
+
+    const Maps& GetMaps() const noexcept {
+        return maps_;
+    }
+
+    const Map* FindMap(const Map::Id& id) const noexcept {
+        if (auto it = map_id_to_index_.find(id); it != map_id_to_index_.end()) {
+            return &maps_.at(it->second);
+        }
+        return nullptr;
+    }
+
+  private:
+    using MapIdHasher = utils::TaggedHasher<Map::Id>;
+    using MapIdToIndex = std::unordered_map<Map::Id, size_t, MapIdHasher>;
+
+    std::vector<Map> maps_;
+    MapIdToIndex map_id_to_index_;
+};
+
+}  // namespace model
---
+++ sprint2/problems/move_players/solution/src/web/utils.cpp
@@ -0,0 +1,101 @@
+#define BOOST_BEAST_USE_STD_STRING_VIEW
+
+#include "web/utils.h"
+#include "web/content_type.h"
+#include "logger/json.h"
+
+#include <boost/beast/core/string.hpp>
+
+#include <sstream>
+#include <iomanip>
+
+namespace web {
+
+namespace beast = boost::beast;
+namespace json = boost::json;
+namespace logging = boost::log;
+
+std::string DecodeUrl(std::string_view url) {
+    std::string result;
+
+    for (size_t pos = 0; pos < url.size(); ++pos) {
+        if (url[pos] != '%') {
+            result += url[pos];
+            continue;
+        }
+
+        if (pos >= url.size() - 2) {
+            break;
+        }
+
+        std::stringstream ss;
+        ss << std::hex << url.substr(pos + 1, 2);
+        uint16_t symbol;
+        ss >> symbol;
+
+        result += static_cast<char>(symbol);
+        pos += 2;
+    }
+
+    return result;
+}
+
+std::string_view GetMimeType(std::string_view path) {
+    const std::string_view ext = [&path] {
+        const size_t pos = path.rfind(".");
+        if (pos == path.npos) {
+            return std::string_view();
+        }
+        return path.substr(pos);
+    }();
+
+    const auto is_ext = [&ext](const auto&... args) {
+        return (beast::iequals(ext, args) || ...);
+    };
+
+    if (is_ext(".htm", ".html")) {
+        return ContentType::text::html;
+    } else if (is_ext(".css")) {
+        return ContentType::text::css;
+    } else if (is_ext(".txt")) {
+        return ContentType::text::plain;
+    } else if (is_ext(".js")) {
+        return ContentType::text::javascript;
+    } else if (is_ext(".json")) {
+        return ContentType::application::json;
+    } else if (is_ext(".xml")) {
+        return ContentType::application::xml;
+    } else if (is_ext(".png")) {
+        return ContentType::image::png;
+    } else if (is_ext(".jpg", ".jpe", ".jpeg")) {
+        return ContentType::image::jpeg;
+    } else if (is_ext(".gif")) {
+        return ContentType::image::gif;
+    } else if (is_ext(".bmp")) {
+        return ContentType::image::bmp;
+    } else if (is_ext(".ico")) {
+        return ContentType::image::icon;
+    } else if (is_ext(".tiff", ".tif")) {
+        return ContentType::image::tiff;
+    } else if (is_ext(".svg", ".svgz")) {
+        return ContentType::image::svg;
+    } else if (is_ext(".mp3")) {
+        return ContentType::audio::mpeg;
+    } else {
+        return ContentType::application::octet_stream;
+    }
+}
+
+void ReportError(sys::error_code ec, std::string_view where) {
+    BOOST_LOG_TRIVIAL(error) << logging::add_value(
+                                    logger::json::additional_data,
+                                    json::value {
+                                        {"code", ec.value()},
+                                        {"text", ec.message()},
+                                        {"where", where},
+                                    }
+                                )
+                             << "error";
+}
+
+}  // namespace web
